375542912dc2fcd0ed812d8a22d487405337b5d0
iamgroup2 <iamgroup2>
2018-09-21 15:26:48 +0000

    Export: export during exercise

---
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigsgroup2.lua
@@ -0,0 +1,29 @@
+local groupName = 'group2'
+local envVariable = 'serverType'..groupName
+local serverType = os.getenv(envVariable)
+
+local iucConf = {}
+
+iucConf.dev = {
+   -- Configurations for patient csv File
+   ['patientCsvPath'] = '/home/centos/iuc/group2/',
+   ['patientCsvFile'] = 'PatientData.txt',
+	
+   -- Configurations for patient DB file
+   ['patientDbPath'] = '/home/centos/iuc/group2/',
+   ['patientDbFile'] = 'PatientData.sqlite'
+}
+
+
+iucConf.prod = {
+   -- Configurations for patient csv File
+   ['patientCsvPath'] = '/home/centos/iuc/prod/group2/',
+   ['patientCsvFile'] = 'PatientData.txt',
+	
+   -- Configurations for patient DB file
+   ['patientDbPath'] = '/home/centos/iuc/prod/group2/',
+   ['patientDbFile'] = 'PatientData.sqlite'
+}
+
+
+return iucConf[serverType]
